7a35cbf62583bcfa32c8f4a6bed3d4eff9c864f3
System Administrator <root@Mark-Joness-MacBook-Pro-2.local>
2014-12-17 05:53:38 -0800

    Moved the object files into an object folder for further reference, as well as created the skeleton for the FileUtil class for file management in this program.

---
+++ FateCoreSystemCharacterCreation/utils/FileUtil.java
@@ -0,0 +1,16 @@
+//This interface will include the methods to open and edit text files based on
+//the values of the Character class and it's child classes. 
+
+public class FileUtil
+{
+	public FileUtil()
+	{
+
+	}//end EVC
+
+	@Override
+	public String toString()
+	{
+		return "";
+	}//end toString
+}//end FileUtil 
